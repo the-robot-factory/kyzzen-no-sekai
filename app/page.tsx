85e7ea4005a99891992272980410a126b283786f
miriam-samuels <medomiriam@gmail.com>
2024-12-14 03:24:19 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,74 +1,51 @@
-
-import { Anton } from 'next/font/google';
+import {Anton} from 'next/font/google';
 import styles from './page.module.css';
-import Image from "next/image";
+import Image from 'next/image';
 
 const anton = Anton({
-  subsets: ["latin"],
-  weight: "400"
-})
+  subsets: ['latin'],
+  weight: '400',
+});
 
 export default function Home() {
   return (
     <div>
       <main>
-
-        <div className={styles.hero + " " + anton.className}>
-          <Image
-            className={styles.hero_girl}
-            src="/images/hero-girl.png"
-            alt="girl"
-            width={373}
-            height={590}
-            priority
-          />
-          <h1><span>KYZZEN</span><br />NO SEKAI</h1>
-          <Image
-            className={styles.hero_boy}
-            src="/images/hero-boy.png"
-            alt="boy"
-            width={482}
-            height={660}
-            priority
-          />
-
+        <div className={styles.hero + ' ' + anton.className}>
+          <Image className={styles.hero_girl} src="/images/hero-girl.png" alt="girl" width={373} height={590} priority />
+          <h1>
+            <span>KYZZEN</span>
+            <br />
+            NO SEKAI
+          </h1>
+          <Image className={styles.hero_boy} src="/images/hero-boy.png" alt="boy" width={482} height={660} priority />
         </div>
         <div className={styles.panel}>
           <div className={styles.about}>
             <div className={styles.panel_img}>
-              <Image
-                src="/images/body-1.png"
-                alt="X"
-                width={500}
-                height={500}
-                priority
-                color='white'
-                className={styles.bg}
-              />
+              <Image src="/images/body-1.png" alt="X" width={500} height={500} priority color="white" className={styles.bg} />
             </div>
             <div className={styles.panel_border}></div>
             <div className={styles.panel_text}>
               <h2 className={anton.className}>About</h2>
-              <p>KNS is a collection of 5,000 digital avatars set in the “World of Kyzzen”, which is a fictional representation of the NFT space</p>
+              <p>
+                KNS is a collection of 5,000 digital avatars set in the “World of Kyzzen”, which is a fictional representation of
+                the NFT space
+              </p>
               <p>Kyzzen is the largest NFT data and educational platform on the Solana blockchain.</p>
             </div>
           </div>
           <div className={styles.art}>
             <div className={styles.panel_img}>
-              <Image
-                src="/images/body-1.png"
-                alt="X"
-                width={500}
-                height={500}
-                priority
-                color='white'
-                className={styles.bg}
-              />
+              <Image src="/images/body-1.png" alt="X" width={500} height={500} priority color="white" className={styles.bg} />
             </div>
             <div className={styles.panel_border}></div>
             <div className={styles.panel_text}>
               <h2 className={anton.className}>The Art</h2>
-              <p>KNS is a groundbreaking experiment that fuses cutting-edge AI technology with the dynamic, modern manga style of our artist, MIDORIMAE, unlocking a new level of creativity and pushing artistic boundaries like never before!</p>
+              <p>
+                KNS is a groundbreaking experiment that fuses cutting-edge AI technology with the dynamic, modern manga style of
+                our artist, MIDORIMAE, unlocking a new level of creativity and pushing artistic boundaries like never before!
+              </p>
             </div>
           </div>
         </div>
@@ -76,38 +53,16 @@ export default function Home() {
         <div className={styles.panel}>
           <div className={styles.founder}>
             <div className={styles.panel_img}>
-              <Image
-                src="/images/body-3.png"
-                alt="X"
-                width={500}
-                height={500}
-                priority
-                color='white'
-                className={styles.bg}
-              />
+              <Image src="/images/body-3.png" alt="X" width={500} height={500} priority color="white" className={styles.bg} />
             </div>
             <div className={styles.panel_border}></div>
             <div className={styles.panel_text}>
               <h2 className={anton.className}>Founder</h2>
               <div className={styles.founder_detail}>
-                <Image
-                  className={styles.hero_girl}
-                  src="/images/founder.png"
-                  alt="girl"
-                  width={60}
-                  height={60}
-                  priority
-                />
+                <Image className={styles.hero_girl} src="/images/founder.png" alt="girl" width={60} height={60} priority />
                 <div>
                   <b>OhMeOhMy</b> <br />
-                  <Image
-                    src="/svg/x.svg"
-                    alt="X"
-                    width={25}
-                    height={25}
-                    priority
-                    color='white'
-                  />
+                  <Image src="/svg/x.svg" alt="X" width={25} height={25} priority color="white" />
                 </div>
               </div>
               <ul>
@@ -120,15 +75,7 @@ export default function Home() {
           </div>
           <div className={styles.utility}>
             <div className={styles.panel_img}>
-              <Image
-                src="/images/body-4.png"
-                alt="X"
-                width={500}
-                height={500}
-                priority
-                color='white'
-                className={styles.bg}
-              />
+              <Image src="/images/body-4.png" alt="X" width={500} height={500} priority color="white" className={styles.bg} />
             </div>
             <div className={styles.panel_border}></div>
             <div className={styles.panel_text}>
@@ -148,9 +95,15 @@ export default function Home() {
           <div className={styles.panel_text}>
             <h2 className={anton.className}>Launch Details</h2>
             <ul>
-              <li><b>Date</b>&nbsp; TBD</li>
-              <li><b>Supply</b>&nbsp; 5,000</li>
-              <li><b>Price</b>&nbsp; TBD</li>
+              <li>
+                <b>Date</b>&nbsp; TBD
+              </li>
+              <li>
+                <b>Supply</b>&nbsp; 5,000
+              </li>
+              <li>
+                <b>Price</b>&nbsp; TBD
+              </li>
             </ul>
             <button>Learn More</button>
           </div>
@@ -159,5 +112,3 @@ export default function Home() {
     </div>
   );
 }
-
-
