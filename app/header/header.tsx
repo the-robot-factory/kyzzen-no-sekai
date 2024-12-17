f39726284e5cd7d345c07ed11a152696a2456f20
miriam-samuels <medomiriam@gmail.com>
2024-12-17 09:13:14 +0100

    .

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Image from 'next/image';
 import styles from './header.module.css';
 
