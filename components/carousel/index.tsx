cf8401eceeeeb0ba936915e72c014ddeed06ae86
miriam-samuels <medomiriam@gmail.com>
2025-01-24 23:51:12 +0100

    chore: tab view

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -1,12 +1,12 @@
 'use client';
-import React, {ReactNode, useState} from 'react';
+import React, { ReactNode, useState } from 'react';
 import styles from './index.module.css';
 
 interface ICarousel {
   slides: ReactNode[];
 }
 
-const CustomCarousel: React.FC<ICarousel> = ({slides}) => {
+const CustomCarousel: React.FC<ICarousel> = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNext = () => {
@@ -22,13 +22,17 @@ const CustomCarousel: React.FC<ICarousel> = ({slides}) => {
   return (
     <div className={styles.carousel}>
       <button className={styles.navButton} onClick={handlePrev}>
-        &#8592;
+      <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fillRule="evenodd" clipRule="evenodd" d="M15.2071 6.29289C15.5976 6.68342 15.5976 7.31658 15.2071 7.70711L10.9142 12L15.2071 16.2929C15.5976 16.6834 15.5976 17.3166 15.2071 17.7071C14.8166 18.0976 14.1834 18.0976 13.7929 17.7071L8.79289 12.7071C8.40237 12.3166 8.40237 11.6834 8.79289 11.2929L13.7929 6.29289C14.1834 5.90237 14.8166 5.90237 15.2071 6.29289Z" fill="currentColor"/>
+</svg>
       </button>
 
       <div className={styles.slide}>{slides[currentIndex]}</div>
 
       <button className={styles.navButton} onClick={handleNext}>
-        &#8594;
+        <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" clipRule="evenodd" d="M8.79289 6.29289C9.18342 5.90237 9.81658 5.90237 10.2071 6.29289L15.2071 11.2929C15.5976 11.6834 15.5976 12.3166 15.2071 12.7071L10.2071 17.7071C9.81658 18.0976 9.18342 18.0976 8.79289 17.7071C8.40237 17.3166 8.40237 16.6834 8.79289 16.2929L13.0858 12L8.79289 7.70711C8.40237 7.31658 8.40237 6.68342 8.79289 6.29289Z" fill="currentColor" />
+        </svg>
       </button>
     </div>
   );
