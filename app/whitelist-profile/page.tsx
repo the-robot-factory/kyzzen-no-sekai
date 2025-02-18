13998860bec84d0970d08a25779e197af27385e3
miriam-samuels <medomiriam@gmail.com>
2025-02-18 15:12:25 +0100

    formatting

--- app/whitelist-profile/page.tsx
+++ app/whitelist-profile/page.tsx
@@ -3,45 +3,45 @@
 import Table from '@/components/table/table';
 import styles from './page.module.css';
 import Image from 'next/image';
-import { SOCIAL_PROVIDERS } from '@/types/enums';
-import { useMemo } from 'react';
-import { SOCIALS } from '@/types/types';
-import { screens } from '@/constants/screen';
+import {SOCIAL_PROVIDERS} from '@/types/enums';
+import {useMemo} from 'react';
+import {SOCIALS} from '@/types/types';
+import {screens} from '@/constants/screen';
 import Tooltip from '@/components/tooltip/tooltip';
 import Link from 'next/link';
-import { useUser } from '@/context/user';
-import { useFetchProfile } from '@/api/profile';
+import {useUser} from '@/context/user';
+import {useFetchProfile} from '@/api/profile';
 
 const WhitelistProfile = () => {
-  const { userSession } = useUser();
+  const {userSession} = useUser();
 
-  const { data, isFetching }: any = useFetchProfile(userSession?.id ?? '');
+  const {data, isFetching}: any = useFetchProfile(userSession?.id ?? '');
 
   const profile = useMemo(() => {
     return data?.profile?.nodes[0];
   }, [data]);
 
   const networkMembers = [
-    { name: 'JemmyJemm', role: 'MonkeDAO Co-Founder', avatar: ' ' },
-    { name: 'CryptoApe', role: 'MultiChain Advisors, Founder', avatar: ' ' },
-    { name: 'PapiChuloGrim', role: 'MultiChain Advisors CMO', avatar: ' ' },
-    { name: 'Zeneca', role: 'Zen Academy Founder', avatar: ' ' },
-    { name: 'Genuine Articles', role: 'GeckoDAO Founder', avatar: ' ' },
-    { name: 'Voshy', role: 'GREED Academy Founder', avatar: ' ' },
-    { name: 'unjustmouse', role: 'GREED Academy Head of Education', avatar: ' ' },
-    { name: 'Turnt Up Dylan', role: 'Dead King Society Founder', avatar: ' ' },
-    { name: 'draxx.ts.sol', role: 'Famous Fox Federation Co-Founder', avatar: ' ' },
-    { name: 'HoTsAuCe', role: 'NFT Radar Community Manager', avatar: ' ' },
-    { name: 'Nom', role: 'Bonk Co-Founder', avatar: ' ' },
-    { name: 'Kais', role: 'Okay Bears Founder', avatar: ' ' },
-    { name: 'Easy', role: 'BoDoggos Founder', avatar: ' ' },
-    { name: 'Solarians', role: 'RoboDAO Council Member', avatar: ' ' },
-    { name: 'NFP', role: 'Pesky Penguins Co-Founder', avatar: ' ' },
-    { name: 'Solana Sensei', role: 'Sensei / Namaste Founder', avatar: ' ' },
-    { name: 'Timon', role: 'Meerkat Millionaires Founder', avatar: ' ' },
-    { name: 'DogeFather', role: 'Doge Capital Founder', avatar: ' ' },
-    { name: 'DJ Trix', role: 'Fearless Bulls Club Founder', avatar: ' ' },
-    { name: 'SOK', role: 'Raposa Founder', avatar: ' ' },
+    {name: 'JemmyJemm', role: 'MonkeDAO Co-Founder', avatar: ' '},
+    {name: 'CryptoApe', role: 'MultiChain Advisors, Founder', avatar: ' '},
+    {name: 'PapiChuloGrim', role: 'MultiChain Advisors CMO', avatar: ' '},
+    {name: 'Zeneca', role: 'Zen Academy Founder', avatar: ' '},
+    {name: 'Genuine Articles', role: 'GeckoDAO Founder', avatar: ' '},
+    {name: 'Voshy', role: 'GREED Academy Founder', avatar: ' '},
+    {name: 'unjustmouse', role: 'GREED Academy Head of Education', avatar: ' '},
+    {name: 'Turnt Up Dylan', role: 'Dead King Society Founder', avatar: ' '},
+    {name: 'draxx.ts.sol', role: 'Famous Fox Federation Co-Founder', avatar: ' '},
+    {name: 'HoTsAuCe', role: 'NFT Radar Community Manager', avatar: ' '},
+    {name: 'Nom', role: 'Bonk Co-Founder', avatar: ' '},
+    {name: 'Kais', role: 'Okay Bears Founder', avatar: ' '},
+    {name: 'Easy', role: 'BoDoggos Founder', avatar: ' '},
+    {name: 'Solarians', role: 'RoboDAO Council Member', avatar: ' '},
+    {name: 'NFP', role: 'Pesky Penguins Co-Founder', avatar: ' '},
+    {name: 'Solana Sensei', role: 'Sensei / Namaste Founder', avatar: ' '},
+    {name: 'Timon', role: 'Meerkat Millionaires Founder', avatar: ' '},
+    {name: 'DogeFather', role: 'Doge Capital Founder', avatar: ' '},
+    {name: 'DJ Trix', role: 'Fearless Bulls Club Founder', avatar: ' '},
+    {name: 'SOK', role: 'Raposa Founder', avatar: ' '},
   ];
 
   const socials: SOCIALS[] = [
@@ -51,7 +51,14 @@ const WhitelistProfile = () => {
       account: 'Email',
       linkedAccount: profile?.email,
       status: profile?.emailVerified,
-      action: <>Not subscribed to Kyzzen Newsletter yet. <a href="" className={styles.link_action}>Subscribe here</a></>,
+      action: (
+        <>
+          Not subscribed to Kyzzen Newsletter yet.{' '}
+          <a href="" className={styles.link_action}>
+            Subscribe here
+          </a>
+        </>
+      ),
     },
     {
       id: SOCIAL_PROVIDERS.X,
@@ -59,7 +66,14 @@ const WhitelistProfile = () => {
       icon: '/svg/x.svg',
       linkedAccount: profile?.twitterUsername,
       status: profile?.discordVerified,
-      action: <>Not following Kyzzen yet.  <a href="" className={styles.link_action}>Follow Here</a></>,
+      action: (
+        <>
+          Not following Kyzzen yet.{' '}
+          <a href="" className={styles.link_action}>
+            Follow Here
+          </a>
+        </>
+      ),
     },
     {
       id: SOCIAL_PROVIDERS.Discord,
@@ -67,7 +81,14 @@ const WhitelistProfile = () => {
       icon: '/svg/discord.svg',
       linkedAccount: profile?.discordUsername,
       status: profile?.discordVerified,
-      action: <>Haven&apos;t joined discord server yet.  <a href="" className={styles.link_action}>Join here</a></>,
+      action: (
+        <>
+          Haven&apos;t joined discord server yet.{' '}
+          <a href="" className={styles.link_action}>
+            Join here
+          </a>
+        </>
+      ),
     },
     {
       id: SOCIAL_PROVIDERS.Telegram,
@@ -75,7 +96,14 @@ const WhitelistProfile = () => {
       icon: '/svg/telegram.svg',
       linkedAccount: profile?.telegramUsername,
       status: profile?.telegramVerified,
-      action: <>Not using Kyzzen’s Telegram Bot yet. <a href="" className={styles.link_action}>Try it out here.</a></>,
+      action: (
+        <>
+          Not using Kyzzen’s Telegram Bot yet.{' '}
+          <a href="" className={styles.link_action}>
+            Try it out here.
+          </a>
+        </>
+      ),
     },
     {
       id: SOCIAL_PROVIDERS.YouTube,
@@ -83,7 +111,14 @@ const WhitelistProfile = () => {
       icon: '/svg/youtube.svg',
       linkedAccount: profile?.youtubeUsername,
       status: profile?.youtubeVerified,
-      action: <>Not following Kyzzen yet.  <a href="" className={styles.link_action}>Follow here</a></>,
+      action: (
+        <>
+          Not following Kyzzen yet.{' '}
+          <a href="" className={styles.link_action}>
+            Follow here
+          </a>
+        </>
+      ),
     },
   ];
 
@@ -127,11 +162,11 @@ const WhitelistProfile = () => {
             <button className={styles.wallet_button}>Link / Unlink Connections</button>
           </div>
           <Table
-            header={[{ name: 'Account' }, { name: 'Linked Account' }, { name: 'Status' }]}
+            header={[{name: 'Account'}, {name: 'Linked Account'}, {name: 'Status'}]}
             body={socials}
             isRow
             Row={SocialRow}
-            style={{ margin: '0 auto', height: 'fit-content' }}
+            style={{margin: '0 auto', height: 'fit-content'}}
             className={styles.connection_table}
           />
         </div>
@@ -148,11 +183,11 @@ const WhitelistProfile = () => {
             <button className={styles.wallet_button}>Add / Unlink Wallets</button>
           </div>
           <Table
-            header={[{ name: 'Network' }, { name: 'Address' }, { name: '' }]}
+            header={[{name: 'Network'}, {name: 'Address'}, {name: ''}]}
             body={profile?.wallets}
             isRow
             Row={WalletRow}
-            style={{ margin: '0 auto', height: 'fit-content' }}
+            style={{margin: '0 auto', height: 'fit-content'}}
             className={styles.connection_table}
           />
         </div>
@@ -221,7 +256,7 @@ const WhitelistProfile = () => {
 
 export default WhitelistProfile;
 
-const SocialRow = ({ data }: { data: SOCIALS }) => {
+const SocialRow = ({data}: {data: SOCIALS}) => {
   return (
     <tr className={styles.row}>
       <td>
@@ -236,7 +271,7 @@ const SocialRow = ({ data }: { data: SOCIALS }) => {
   );
 };
 
-const WalletRow = ({ data }: { data: string }) => {
+const WalletRow = ({data}: {data: string}) => {
   const isPrimary = (addr: string) => {
     return addr.includes('primary:');
   };
