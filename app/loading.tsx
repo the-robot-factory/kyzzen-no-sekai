6b4e69c99be5f3fb0397a4d3f366df8c44283d3a
miriam-samuels <medomiriam@gmail.com>
2025-08-14 09:36:24 +0100

    .

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function Loading() {
-    return (
-        <div className="loader">
-            <div className="loader-det">
-                <p>{'Hold on a bit...'}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="loader">
+      <div className="loader-det">
+        <p>{'Hold on a bit...'}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Loading
+export default Loading;
