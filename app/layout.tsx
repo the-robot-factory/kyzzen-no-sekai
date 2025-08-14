64dfa6c81b1afe80bed17132979c4630156dab8b
miriam-samuels <medomiriam@gmail.com>
2025-08-14 09:24:06 +0100

    scroll bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 // import type {Metadata} from 'next';
-import {Open_Sans} from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 import './globals.css';
 import Header from '../components/header/header';
-import {ToastContainer, Slide} from 'react-toastify';
+import { ToastContainer, Slide } from 'react-toastify';
 import Providers from './providers';
 
 const sans = Open_Sans({
@@ -21,24 +21,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${sans.className} con`}>
-        <Providers>
-          <Header />
-          {children}
-        </Providers>
-        <ToastContainer
-          position="bottom-center"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-          transition={Slide}
-        />
+      <body className={`${sans.className}`}>
+        <div className='con'>
+          <Providers>
+            <Header />
+            {children}
+          </Providers>
+          <ToastContainer
+            position="bottom-center"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+            transition={Slide}
+          />
+        </div>
+
       </body>
     </html>
   );
